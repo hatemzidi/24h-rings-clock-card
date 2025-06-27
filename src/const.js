533530ed48802bb75e042df7e3007f63068a471e
hatem.zidi <hatem.zidi@gmail.com>
2025-06-27 09:03:10 +0200

    ci() : automate the version to the tag's version

--- src/const.js
+++ src/const.js
@@ -1,7 +1,7 @@
 
 // Constants (Defaults, Icons, Metadata)
 export const CARD_DESCRIPTION = 'Enhanced 24-hours analog clock with time ranges, sun information, custom markers, and more display options.';
-export const CARD_VERSION = '0.3.0';
+export const CARD_VERSION = '0.0.0';
 export const DEFAULT_SUNRISE_ICON_TEXT = '↑';
 export const DEFAULT_SUNSET_ICON_TEXT = '↓';
 export const DEFAULT_CUSTOM_MARKER_ICON_TEXT = '•';
